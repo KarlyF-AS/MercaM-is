1b389e01395a284e683e606102c9f433d20b3ec9
DanielRTato <drodrigueztato@danielcastelao.org>
2025-06-02 19:09:21 +0200

    Clases vacias

---
+++ Lista_UnidadFamiliar.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Map;
+
+public class Lista_UnidadFamiliar {
+
+    private int id;
+    private String nombre;
+    private String codigo;
+    private List<Usuario> miembros;
+    private Map<Integer, Producto> productos;
+
+
+
+}
