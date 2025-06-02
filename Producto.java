edc95bd12d61c37204302e6dac3a600a1ae103ed
DanielRTato <drodrigueztato@danielcastelao.org>
2025-06-02 19:20:00 +0200

    clases minimas necesarias

--- Producto.java
+++ Producto.java
@@ -1,2 +1,11 @@
+import java.util.Set;
+
 public class Producto {
+    private int codigoBarras;
+    private String nombre;
+    private String marca;
+    private int puntuacion;
+    private double precio;
+    private String categoria;
+    private Set<Supermercado> supermercados;
 }
