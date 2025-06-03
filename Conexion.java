24edbf903319c74d3471a0d00f94c6c6d20e5804
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-06-03 11:18:16 +0200

    creando la clasle conexion para conectarse a la base de datos

---
+++ Conexion.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static Connection abrir() throws SQLException {
+        String url  = "jdbc:postgresql://aws-0-eu-west-2.pooler.supabase.com:6543/postgres";
+        String user = "postgres";
+        String pass = "Mercamais123.";
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
