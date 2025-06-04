f9e5dec3a17419b9619f49584708f6c07297652d
DanielRTato <drodrigueztato@danielcastelao.org>
2025-06-04 09:59:37 +0200

    Controlador

--- Controlador.java
+++ Controlador.java
@@ -4,5 +4,42 @@ public class Controlador {
         return Model.validarLogin(correo, contraseña);
     }
 
-    public static existeUsuario(nombre)
+    public static existeEmail(String nombreEmail) {
+        return Model.existeEmail(nombreEmail);
+    }
+    public static registrarUsuario (String nombre, String correo, String contraseña) {
+        return Model.registrarUsuario(nombre, correo, contraseña);
+    }
+
+    public static obtenerUnidadFamiliar(String usuarioActual) {
+        return Model.obtenerUnidadFamiliar(usuarioActual);
+    }
+
+    public static unirseUnidadFamiliar(String codigo, String usuarioActual) {
+        return Model.unirseUnidadFamiliar(codigo, usuarioActual);
+    }
+
+    public static crearUnidadFamiliar(String usuarioActual, String nombreUnidadFamiliar) {
+        return Model.crearUnidadFamiliar(usuarioActual, nombreUnidadFamiliar);
+    }
+
+    public static obtenerTodosProductos(String unidadActual) {
+        return Model.obtenerTodosProductos(unidadActual);
+    }
+
+    public static obtenerCategorias () {
+        return Model.obtenerCategorias();
+    }
+
+    public static obtenerProductosPorSubcategoria(String subcategoria) {
+        return Model.obtenerProductosPorSubcategoria(subcategoria);
+    }
+
+    public static obtenerMarcas(){
+        return Model.obtenerMarcas();
+    }
+
+    public static obtenerProductosPorMarca(marca) {
+        return Model.obtenerProductosPorMarca(marca);
+    }
 }
